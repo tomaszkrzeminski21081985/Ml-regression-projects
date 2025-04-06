b3fa48f2fd44f4eb92386b1046918d74fe3f64ce
TomaszKrzeminski <tomaszkrzeminski21081985@gmail.com>
2025-04-06 12:58:19 +0200

    get data

---
+++ .vs/MLRegressions/dataStatistics.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+class Statistics:
+
+    def show_Statistics(self,df_Dataset):
+        # print(df_Dataset.shape)
+        # print("\n")
+        # print(df_Dataset.info())
+        # print("\n")
+        # categorical = [var for var in df_Dataset.columns if df_Dataset[var].dtype=='O']
+
+        # print('There are {} categorical variables\n'.format(len(categorical)))
+
+        # print('The categorical variables are :', categorical)
+        
+        
+        # print(df_Dataset[categorical].head())
+        
+        # for var in categorical:
+        #  print(f"Categorical with missing values: {var} - {df_Dataset[var].isnull().sum()}")
+           
+        # # check for cardinality in categorical variables
+
+        # for var in categorical:
+            
+        #     print(var, ' contains ', len(df_Dataset[var].unique()), ' labels')
+        # print(df_Dataset.head())
+        # print("\n")
+        # print(df_Dataset.columns)
+        # print("\n") 
+        # print(df_Dataset.dtypes)
+        # print(df_Dataset.shape)
+       
+    #    numerical = [var for var in df_Dataset.columns if df_Dataset[var].dtype!='O']
+
+    #    print('There are {} numerical variables\n'.format(len(numerical)))
+
+    #    print('The numerical variables are :', numerical)
+       
+    #    print(df_Dataset[numerical].head(10).to_string(index=False))
+       
+    #    print('Missing values in numerical types: ', df_Dataset[numerical].isnull().sum())
+       
+       print(df_Dataset.describe())
---
+++ .vs/MLRegressions/weather.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, RobustScaler
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+class Weather:
+    
+                    
+   def get_data(self):
+        data = 'weatherAUS.csv'
+        self.df_Dataset = pd.read_csv(data)
+        self.df_Dataset['Date'] = pd.to_datetime(self.df_Dataset['Date'])
+        
+        self.df_Dataset['Year'] =  self.df_Dataset['Date'].dt.year
+        self.df_Dataset['Month'] = self.df_Dataset['Date'].dt.month
+        self.df_Dataset['Day'] = self.df_Dataset['Date'].dt.day
+        self.df_Dataset.drop('Date', axis=1, inplace = True)
+        
+        print(self.df_Dataset.columns)
+        print(self.df_Dataset.info())
+        
+        
+        self.change_to_hotEncoding()      
+        self.change_numerical()  
+        
+     #    self.df_Dataset.drop(['RISK_MM'])
+        return self.df_Dataset
+   
+   
+   def change_to_hotEncoding(self):
+    encoder = OneHotEncoder()
+    categorical = [var for var in self.df_Dataset.columns if self.df_Dataset[var].dtype=='O' and var != 'RainTomorrow']
+    encoded_data = encoder.fit_transform(self.df_Dataset[categorical])
+    encoded_df = pd.DataFrame(encoded_data.toarray())
+    encoded_df.columns = encoder.get_feature_names_out()
+    self.df_Dataset = pd.concat([self.df_Dataset.drop(categorical, axis=1), encoded_df], axis=1)
+    
+    
+   def change_numerical(self):
+    numerical = [var for var in self.df_Dataset.columns if self.df_Dataset[var].dtype!='O']
+    self.df_Dataset[numerical] = self.df_Dataset[numerical].replace([np.inf, -np.inf], np.nan)
+    self.df_Dataset[numerical] = self.df_Dataset[numerical].fillna(self.df_Dataset[numerical].mean())
+    for var in numerical:
+        self.df_Dataset[var] = np.log(self.df_Dataset[var]+ 1)
+        self.df_Dataset[var] = self.df_Dataset[var].replace([np.inf, -np.inf], np.nan)  # Add this line
+        self.df_Dataset[var] = self.df_Dataset[var].fillna(self.df_Dataset[var].mean())  # Add this line
+        
+    targetScaller=RobustScaler()        
+        
+    self.df_Dataset[numerical] = targetScaller.fit_transform(self.df_Dataset[numerical])
